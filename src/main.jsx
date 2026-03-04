80199889d42606ca1970cfa8d23a9117deb621bb
Tony Masis <tonnymasis@gmail.com>
2026-03-04 16:20:39 -0600

    CRITICAL FIX: Configure React Router basename for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/alby-lodge-cr">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
